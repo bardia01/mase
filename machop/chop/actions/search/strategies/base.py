b6dd78431c697b661dd8e9a3512975437996ad8e
bardia01 <bardia.mzad@gmail.com>
2024-02-05 16:03:03 +0000

    a

--- machop/chop/actions/search/strategies/base.py
+++ machop/chop/actions/search/strategies/base.py
@@ -90,189 +90,189 @@ class SearchStrategyBase:
         """
         raise NotImplementedError()
 
-class bardia_exhaustive_search(SearchStrategyBase):
-    def _post_init_setup(self):
-        self.sum_scaled_metrics = self.config["setup"]["sum_scaled_metrics"]
-        self.metric_names = list(sorted(self.config["metrics"].keys()))
-        if not self.sum_scaled_metrics:
-            self.directions = [
-                self.config["metrics"][k]["direction"] for k in self.metric_names
-            ]
-        else:
-            self.direction = self.config["setup"]["direction"]
+# class bardia_exhaustive_search(SearchStrategyBase):
+#     def _post_init_setup(self):
+#         self.sum_scaled_metrics = self.config["setup"]["sum_scaled_metrics"]
+#         self.metric_names = list(sorted(self.config["metrics"].keys()))
+#         if not self.sum_scaled_metrics:
+#             self.directions = [
+#                 self.config["metrics"][k]["direction"] for k in self.metric_names
+#             ]
+#         else:
+#             self.direction = self.config["setup"]["direction"]
 
     
-    def compute_software_metrics(self, model, sampled_config: dict, is_eval_mode: bool):
-        # note that model can be mase_graph or nn.Module
-        metrics = {}
-        if is_eval_mode:
-            with torch.no_grad():
-                for runner in self.sw_runner:
-                    metrics |= runner(self.data_module, model, sampled_config)
-        else:
-            for runner in self.sw_runner:
-                metrics |= runner(self.data_module, model, sampled_config)
-        return metrics
+#     def compute_software_metrics(self, model, sampled_config: dict, is_eval_mode: bool):
+#         # note that model can be mase_graph or nn.Module
+#         metrics = {}
+#         if is_eval_mode:
+#             with torch.no_grad():
+#                 for runner in self.sw_runner:
+#                     metrics |= runner(self.data_module, model, sampled_config)
+#         else:
+#             for runner in self.sw_runner:
+#                 metrics |= runner(self.data_module, model, sampled_config)
+#         return metrics
 
-    def compute_hardware_metrics(self, model, sampled_config, is_eval_mode: bool):
-        metrics = {}
-        if is_eval_mode:
-            with torch.no_grad():
-                for runner in self.hw_runner:
-                    metrics |= runner(self.data_module, model, sampled_config)
-        else:
-            for runner in self.hw_runner:
-                metrics |= runner(self.data_module, model, sampled_config)
-        return metrics
+#     def compute_hardware_metrics(self, model, sampled_config, is_eval_mode: bool):
+#         metrics = {}
+#         if is_eval_mode:
+#             with torch.no_grad():
+#                 for runner in self.hw_runner:
+#                     metrics |= runner(self.data_module, model, sampled_config)
+#         else:
+#             for runner in self.hw_runner:
+#                 metrics |= runner(self.data_module, model, sampled_config)
+#         return metrics
 
-    def objective(self, search_space):
-        sampled_indexes = {}
+#     def objective(self, search_space):
+#         sampled_indexes = {}
 
-        software_metrics = self.compute_software_metrics(
-            model, sampled_config, is_eval_mode
-        )
-        hardware_metrics = self.compute_hardware_metrics(
-            model, sampled_config, is_eval_mode
-        )
-        metrics = software_metrics | hardware_metrics
-        scaled_metrics = {}
-        for metric_name in self.metric_names:
-            scaled_metrics[metric_name] = (
-                self.config["metrics"][metric_name]["scale"] * metrics[metric_name]
-            )
+#         software_metrics = self.compute_software_metrics(
+#             model, sampled_config, is_eval_mode
+#         )
+#         hardware_metrics = self.compute_hardware_metrics(
+#             model, sampled_config, is_eval_mode
+#         )
+#         metrics = software_metrics | hardware_metrics
+#         scaled_metrics = {}
+#         for metric_name in self.metric_names:
+#             scaled_metrics[metric_name] = (
+#                 self.config["metrics"][metric_name]["scale"] * metrics[metric_name]
+#             )
 
-        if not self.sum_scaled_metrics:
-            return list(scaled_metrics.values())
-        else:
-            return sum(scaled_metrics.values())
+#         if not self.sum_scaled_metrics:
+#             return list(scaled_metrics.values())
+#         else:
+#             return sum(scaled_metrics.values())
 
-    def search(self, search_space) -> optuna.study.Study:
+#     def search(self, search_space) -> optuna.study.Study:
         
-        if not self.sum_scaled_metrics:
-            study_kwargs["directions"] = self.directions
-        else:
-            study_kwargs["direction"] = self.direction
+#         if not self.sum_scaled_metrics:
+#             study_kwargs["directions"] = self.directions
+#         else:
+#             study_kwargs["direction"] = self.direction
 
-        a = search_space.choice_lengths_flattened.items()
-            # sampled_indexes[name] = trial.suggest_int(name, 0, length - 1)
-        sampled_config = search_space.flattened_indexes_to_config(sampled_indexes)
+#         a = search_space.choice_lengths_flattened.items()
+#             # sampled_indexes[name] = trial.suggest_int(name, 0, length - 1)
+#         sampled_config = search_space.flattened_indexes_to_config(sampled_indexes)
 
-        is_eval_mode = self.config.get("eval_mode", True)
-        model = search_space.rebuild_model(sampled_config, is_eval_mode)
-        # if isinstance(self.config["setup"].get("pkl_ckpt", None), str):
-        #     study = joblib.load(self.config["setup"]["pkl_ckpt"])
-        #     logger.info(f"Loaded study from {self.config['setup']['pkl_ckpt']}")
-        # else:
-        #     study = optuna.create_study(**study_kwargs)
+#         is_eval_mode = self.config.get("eval_mode", True)
+#         model = search_space.rebuild_model(sampled_config, is_eval_mode)
+#         # if isinstance(self.config["setup"].get("pkl_ckpt", None), str):
+#         #     study = joblib.load(self.config["setup"]["pkl_ckpt"])
+#         #     logger.info(f"Loaded study from {self.config['setup']['pkl_ckpt']}")
+#         # else:
+#         #     study = optuna.create_study(**study_kwargs)
 
-        study.optimize(
-            func=partial(self.objective, search_space=search_space),
-            n_jobs=self.config["setup"]["n_jobs"],
-            n_trials=self.config["setup"]["n_trials"],
-            timeout=self.config["setup"]["timeout"],
-            callbacks=[
-                partial(
-                    callback_save_study,
-                    save_dir=self.save_dir,
-                    save_every_n_trials=self.config["setup"].get(
-                        "save_every_n_trials", 10
-                    ),
-                )
-            ],
-            show_progress_bar=True,
-        )
-        self._save_study(study, self.save_dir / "study.pkl")
-        self._save_search_dataframe(study, search_space, self.save_dir / "log.json")
-        self._save_best(study, self.save_dir / "best.json")
+#         study.optimize(
+#             func=partial(self.objective, search_space=search_space),
+#             n_jobs=self.config["setup"]["n_jobs"],
+#             n_trials=self.config["setup"]["n_trials"],
+#             timeout=self.config["setup"]["timeout"],
+#             callbacks=[
+#                 partial(
+#                     callback_save_study,
+#                     save_dir=self.save_dir,
+#                     save_every_n_trials=self.config["setup"].get(
+#                         "save_every_n_trials", 10
+#                     ),
+#                 )
+#             ],
+#             show_progress_bar=True,
+#         )
+#         self._save_study(study, self.save_dir / "study.pkl")
+#         self._save_search_dataframe(study, search_space, self.save_dir / "log.json")
+#         self._save_best(study, self.save_dir / "best.json")
 
-        for i in search_space.choices_flattened:
-            best_model 
-        return study
+#         for i in search_space.choices_flattened:
+#             best_model 
+#         return study
 
-    @staticmethod
-    def _save_search_dataframe(study: optuna.study.Study, search_space, save_path):
-        df = study.trials_dataframe(
-            attrs=(
-                "number",
-                "value",
-                "user_attrs",
-                "system_attrs",
-                "state",
-                "datetime_start",
-                "datetime_complete",
-                "duration",
-            )
-        )
-        df.to_json(save_path, orient="index", indent=4)
-        return df
+#     @staticmethod
+#     def _save_search_dataframe(study: optuna.study.Study, search_space, save_path):
+#         df = study.trials_dataframe(
+#             attrs=(
+#                 "number",
+#                 "value",
+#                 "user_attrs",
+#                 "system_attrs",
+#                 "state",
+#                 "datetime_start",
+#                 "datetime_complete",
+#                 "duration",
+#             )
+#         )
+#         df.to_json(save_path, orient="index", indent=4)
+#         return df
 
-    @staticmethod
-    def _save_best(study: optuna.study.Study, save_path):
-        df = pd.DataFrame(
-            columns=[
-                "number",
-                "value",
-                "software_metrics",
-                "hardware_metrics",
-                "scaled_metrics",
-                "sampled_config",
-            ]
-        )
-        if study._is_multi_objective:
-            best_trials = study.best_trials
-            for trial in best_trials:
-                row = [
-                    trial.number,
-                    trial.values,
-                    trial.user_attrs["software_metrics"],
-                    trial.user_attrs["hardware_metrics"],
-                    trial.user_attrs["scaled_metrics"],
-                    trial.user_attrs["sampled_config"],
-                ]
-                df.loc[len(df)] = row
-        else:
-            best_trial = study.best_trial
-            row = [
-                best_trial.number,
-                best_trial.value,
-                best_trial.user_attrs["software_metrics"],
-                best_trial.user_attrs["hardware_metrics"],
-                best_trial.user_attrs["scaled_metrics"],
-                best_trial.user_attrs["sampled_config"],
-            ]
-            df.loc[len(df)] = row
-        df.to_json(save_path, orient="index", indent=4)
+#     @staticmethod
+#     def _save_best(study: optuna.study.Study, save_path):
+#         df = pd.DataFrame(
+#             columns=[
+#                 "number",
+#                 "value",
+#                 "software_metrics",
+#                 "hardware_metrics",
+#                 "scaled_metrics",
+#                 "sampled_config",
+#             ]
+#         )
+#         if study._is_multi_objective:
+#             best_trials = study.best_trials
+#             for trial in best_trials:
+#                 row = [
+#                     trial.number,
+#                     trial.values,
+#                     trial.user_attrs["software_metrics"],
+#                     trial.user_attrs["hardware_metrics"],
+#                     trial.user_attrs["scaled_metrics"],
+#                     trial.user_attrs["sampled_config"],
+#                 ]
+#                 df.loc[len(df)] = row
+#         else:
+#             best_trial = study.best_trial
+#             row = [
+#                 best_trial.number,
+#                 best_trial.value,
+#                 best_trial.user_attrs["software_metrics"],
+#                 best_trial.user_attrs["hardware_metrics"],
+#                 best_trial.user_attrs["scaled_metrics"],
+#                 best_trial.user_attrs["sampled_config"],
+#             ]
+#             df.loc[len(df)] = row
+#         df.to_json(save_path, orient="index", indent=4)
 
-        txt = "Best trial(s):\n"
-        df_truncated = df.loc[
-            :, ["number", "software_metrics", "hardware_metrics", "scaled_metrics"]
-        ]
+#         txt = "Best trial(s):\n"
+#         df_truncated = df.loc[
+#             :, ["number", "software_metrics", "hardware_metrics", "scaled_metrics"]
+#         ]
 
-        def beautify_metric(metric: dict):
-            beautified = {}
-            for k, v in metric.items():
-                if isinstance(v, (float, int)):
-                    beautified[k] = round(v, 3)
-                else:
-                    txt = str(v)
-                    if len(txt) > 20:
-                        txt = txt[:20] + "..."
-                    else:
-                        txt = txt[:20]
-                    beautified[k] = txt
-            return beautified
+#         def beautify_metric(metric: dict):
+#             beautified = {}
+#             for k, v in metric.items():
+#                 if isinstance(v, (float, int)):
+#                     beautified[k] = round(v, 3)
+#                 else:
+#                     txt = str(v)
+#                     if len(txt) > 20:
+#                         txt = txt[:20] + "..."
+#                     else:
+#                         txt = txt[:20]
+#                     beautified[k] = txt
+#             return beautified
 
-        df_truncated.loc[
-            :, ["software_metrics", "hardware_metrics", "scaled_metrics"]
-        ] = df_truncated.loc[
-            :, ["software_metrics", "hardware_metrics", "scaled_metrics"]
-        ].map(
-            beautify_metric
-        )
-        txt += tabulate(
-            df_truncated,
-            headers="keys",
-            tablefmt="orgtbl",
-        )
-        logger.info(f"Best trial(s):\n{txt}")
-        return df
+#         df_truncated.loc[
+#             :, ["software_metrics", "hardware_metrics", "scaled_metrics"]
+#         ] = df_truncated.loc[
+#             :, ["software_metrics", "hardware_metrics", "scaled_metrics"]
+#         ].map(
+#             beautify_metric
+#         )
+#         txt += tabulate(
+#             df_truncated,
+#             headers="keys",
+#             tablefmt="orgtbl",
+#         )
+#         logger.info(f"Best trial(s):\n{txt}")
+#         return df
